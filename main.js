b5f2294ff7daeebd5e0e052050a914ba5b310119
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2024-10-22 07:59:47 +0800

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+let nama = 'Dayat';
+const umur = 17;
+
+console.log("nama "+ nama);
+console.log("umur "+ umur);
+
+let GelasA = "Kopi";
+let GelasB = "Teh";
+
+console.log("Isi Gelas A " + GelasA);
